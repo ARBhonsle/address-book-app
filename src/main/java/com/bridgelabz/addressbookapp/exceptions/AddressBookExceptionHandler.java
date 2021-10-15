8478ac243b6a4d72c4e9fe6fe38cc312f6fbb197
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-10-15 21:29:57 +0530

    Added:Exception handling for MethodArgumentNotValidException

--- src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookExceptionHandler.java
+++ src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookExceptionHandler.java
@@ -3,8 +3,13 @@ package com.bridgelabz.addressbookapp.exceptions;
 import com.bridgelabz.addressbookapp.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Address Book exception handler handles exception and give response to bad http requests
@@ -16,8 +21,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class AddressBookExceptionHandler {
     @ExceptionHandler(value = AddressBookException.class)
-    public ResponseEntity<ResponseDto> addressBookExceptionHandler(AddressBookException e) {
-        ResponseDto responseDto = new ResponseDto(e.getMessage(), null);
-        return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ResponseDto> addressBookExceptionHandler(AddressBookException addressBookException) {
+        ResponseDto responseDto = new ResponseDto("Exception while processing REST Request", addressBookException.getMessage());
+        return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> methodArgNotValidExceptionHandler(MethodArgumentNotValidException methodArgumentNotValidException){
+        List<ObjectError> errorList = methodArgumentNotValidException.getBindingResult().getAllErrors();
+        List<String> errMsg =errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
+        ResponseDto responseDto = new ResponseDto("Exception while processing REST Request", errMsg);
+        return new ResponseEntity<>(responseDto,HttpStatus.BAD_REQUEST);
     }
 }
