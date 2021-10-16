631246d1c7aa84879de9d234433fc9854c981b4d
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-10-16 18:44:07 +0530

    Added:Logging pattern, file, root, application levels and modified application.properties

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -1,13 +1,19 @@
 package com.bridgelabz.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+        log.info("Employee Payroll Spring App Started");
+        log.info("Environment", context.getEnvironment().getProperty("environment"));
+        log.info("Employee DB username is {}", context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
